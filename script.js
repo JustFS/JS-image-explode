453a82dd9a9d1dbef99bc43e9f622f66c8b9a5e3
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-05-20 12:17:29 +0200

    added anime.js

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-
-
-requestAnimationFrame(() => {
-  const boxes = document.querySelectorAll(".box");
-
-  function getIntersectionRatio(i) {
-    const a = [window.scrollY, window.scrollY + window.innerHeight];
-    const b = [boxes[i].offsetTop, boxes[i].offsetTop + boxes[i].clientHeight];
-
-    const max = Math.max(a[0], b[0]);
-    const min = Math.min(a[1], b[1]);
-
-    return Math.max(0, (min - max) / (b[1] - b[0]));
-  }
-
-  function onScroll() {
-    const boxes = document.querySelectorAll(".box");
-    for (let i = 0; i < boxes.length; i += 1) {
-      const intersection = getIntersectionRatio(i);
-      const top = boxes[i].offsetTop - window.pageYOffset < 0;
-      boxes[i].firstChild.style.cssText = `
-        transform-origin: ${top ? "center center" : "top center"};
-        position: ${top ? "fixed" : "absolute"};
-        transform: scale(${intersection});
-        opacity: ${intersection};
-      `;
-    }
-    requestAnimationFrame(onScroll);
-  }
-
-  onScroll();
+anime({
+  targets: '.morphing-demo .polymorph',
+  points: [
+    { value: [
+      '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
+      '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
+    },
+    { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+    { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+    { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+  ],
+  easing: 'easeOutQuad',
+  duration: 2000,
+  loop: true
 });
